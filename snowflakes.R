efb14892caeacdd0c12d202aaee810633c67d52b
Daan Seynaeve <daan.seynaeve@openanalytics.eu>
2018-02-22 22:00:35 +0100

    proper 6-fold symmetry

--- snowflakes.R
+++ snowflakes.R
@@ -1,22 +1,26 @@
 # R Snowflakes - Inspired by the nord palette
 # 
-# Authori: Daan Seynaeve 2018
+# Author: Daan Seynaeve
 ###############################################################################
 
+# middle right left
+# down up up
 plotFlake <- function(
     depth = 2,
     x = c(0, 1, -1),
     y = c(-sqrt(3), 0, 0),
-    k = 1000,
+    k = 100000,
     pal = c("black", "black", "black", "white"),
+    sym = TRUE,
     ...) {
   
-  stopifnot(depth > 1)
+  stopifnot(depth > 0)
   
   # recurse in enough triangles
+  # left right bottom middle
   for (j in seq(depth)) {
-    x <- c(x,
-        x + max(x) - min(x),
+    x <- c(x, 
+        x + max(x) - min(x), 
         x + (max(x) - min(x))/2,
         x + (max(x) - min(x))/2)
     y <- c(y,
@@ -29,19 +33,23 @@ plotFlake <- function(
   y <- y - min(y)
   x <- x - max(x) - min(x - max(x))/2
   
-  # mirror around x = 0 and rotate pi/3
-  rot <- function(x, y, theta) {
-    cbind(c(cos(theta), sin(theta)), c(-sin(theta), cos(theta))) %*% rbind(x, y)
+  if (sym) {
+    
+    # mirror around x = 0 and rotate pi/3
+    rot <- function(x, y, theta) {
+      cbind(c(cos(theta), sin(theta)), c(-sin(theta), cos(theta))) %*% rbind(x, y)
+    }
+    M <- rot(-x, y, pi/3)
+    x <- c(x, M[1,])
+    y <- c(y, M[2,])
+    
+    # rotate pi/3 in both directions
+    Ml <- rot(x, y, 2*pi/3)
+    Mr <- rot(x, y, 4*pi/3)
+    x <- c(x, Ml[1,], Mr[1,])
+    y <- c(y, Ml[2,], Mr[2,])
+    
   }
-  M <- rot(-x, y, pi/3)
-  x <- c(x, M[1,])
-  y <- c(y, M[2,])
-  
-  # rotate pi/3 in both directions
-  Ml <- rot(x, y, 2*pi/3)
-  Mr <- rot(x, y, 4*pi/3)
-  x <- c(x, Ml[1,], Mr[1,])
-  y <- c(y, Ml[2,], Mr[2,])
   
   # mirror along y = 2x
   
@@ -53,21 +61,55 @@ plotFlake <- function(
   
   for (j in seq((4 ^ depth) * 6))  {
     p <- ((j*3)-2):(j*3)
-    polygon(x[p], y[p], col = pal[1 + (j %% k %% length(pal))], ...)
+    polygon(x[p], y[p], col = pal[1 + ((j - 1) %% k %% length(pal))], ...)
   }
+  
+  invisible(rbind(x, y))
   
 }
 
-salt <- as.numeric(Sys.time()) %% 17
+
 numPlots <- 10
-depth <- 2
 
 par(mfrow = c(2, numPlots / 2), bg = nord::nord("polarnight")[1])
 for (s in seq(numPlots)) {
   
+  depth <- 3
+  
   # generate a set of responses
-  set.seed(s * salt)
-  sequence <- runif(4 ^ depth) > .5
+  salt <- as.numeric(Sys.time()) %% 17
+  set.seed(salt + s)
+  sequence <- runif(4 ^ depth) > .3
+  
+  # actual snowflake: 6-fold symmetry
+  
+  mir <- function(tri) {
+    if (l <- length(tri) == 1) {
+      tri
+    } else {
+      l <- length(tri)
+      m <- l/2
+      a <- m/2
+      b <- a+m
+      c(mir(tri[(a+1):m]), mir(tri[1:a]),
+          mir(tri[(m+1):b]), mir(tri[(b+1):l]))
+    }
+  }
+  
+  make_mir <- function(tri) {
+    if (l <- length(tri) == 1) {
+      tri
+    } else {
+      l <- length(tri)
+      m <- l/2
+      a <- m/2
+      b <- a+m
+      c(tri[1:a], mir(tri[1:a]),
+          make_mir(tri[(m+1):b]), make_mir(tri[(b+1):l]))
+    }
+  }
+  
+  sequence <- make_mir(sequence)
   
   # repeated palettes
   pal[sequence] <- rep(nord::nord("polarnight"), length(sequence))[which(sequence)]
@@ -75,6 +117,18 @@ for (s in seq(numPlots)) {
   
   # draw a snowflake
   plotFlake(depth, pal = rep(pal, 6), lty = 0)
-  
+ 
 }
+
+## sierpinski pal
+#depth <- 4
+#sierpal <- pal <- c(TRUE, TRUE, TRUE, FALSE)
+#for (j in seq(depth)) {
+#  sierpal <- unlist(lapply(sierpal, function(x) x * pal))
+#}
+#sierpal <- ifelse(sierpal, "black", "white")
+#plotFlake(depth, pal = sierpal, sym = TRUE, lty = 0)
+
+
+
 
